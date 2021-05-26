abc8b66186b8d0d16979aca75f6927ed565755c9
Alex Salman <aalsalma@ucsc.edu>
2021-05-26 02:09:00 -0700

    May 26 2:08

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
-import collections
+# Item Item Based Collaborative Filtering Recommender
+# Alex Salman - aalsalma@ucsc.edu
+# May 26 Fall 2021
 import pandas as pd
 import numpy as np
-import prediction
 from math import sqrt
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn.metrics import pairwise_distances
 from sklearn.neighbors import NearestNeighbors
-# import csv
-from collections import OrderedDict
 
 
 def main():
-    # data = list(csv.reader(open('ratings_Movies_and_TV.csv')))
-    # print(data)
-#################################################################################
-    # data selection and processing
-    # I chose Amazon Movies and TV shows for this HW2
-    data_frame = pd.read_json('reviews_Video_Games_5.json', lines=True)
-    # grouping by reviewerID to split users' ratings 80 training / 20 testing
-    data_frame_group_by_id = data_frame.groupby(data_frame.reviewerID)
-    # print(data_frame_group_by_id.get_group("AQP1VPK16SVWM"))
-    # split to %80 training
-    training_data = data_frame_group_by_id.sample(frac=0.8, random_state=1)
-    training_data = training_data.drop(columns=['reviewerName', 'helpful', 'reviewText', 'summary', 'unixReviewTime', 'reviewTime'])
-    # print(training_data.info())
-    # print(training_data.head())
-    # print(training_data.groupby(training_data.reviewerID).get_group("AQP1VPK16SVWM"))
-    # drop the training set from the main set to get the 20% testing
-#################################################################################
-    # preping testing data for evaluation
-    testing_data = data_frame.drop(training_data.index)
-    testing_data = testing_data.drop(columns=['reviewerName', 'helpful', 'reviewText', 'summary', 'unixReviewTime', 'reviewTime'])
+    # Data selection and processing
+    # Any http://jmcauley.ucsd.edu/data/amazon/ (5-core) works on this algorithm
+    # Reading JSON file and convert it to a pandas dataframe
+    dataframe = pd.read_json('reviews_Video_Games_5.json', lines=True)
+    # Drop all the columns that I am not using in this algorithm
+    dataframe = dataframe.drop(columns=['reviewerName', 'helpful', 'reviewText', 'summary', 'unixReviewTime', 'reviewTime'])
+    # Grouping by reviewerID to split users' ratings 80 training / 20 testing
+    dataframe_group_by_id = dataframe.groupby(dataframe.reviewerID)
+    # Split to %80 training - 80% of each user ratings
+    training_data = dataframe_group_by_id.sample(frac=0.8, random_state=1)
+    # Drop the training set from the main set to get the 20% testing data
+    # Processing testing data to be used for evaluation
+    testing_data = dataframe.drop(training_data.index)
+    # Create a dictionary to include all users in the testing set with their rated items as values for their ids
     testing_data = testing_data.pivot_table(values='overall', index='reviewerID', columns='asin')
     testing_data_user_items = {}
     for i, row in testing_data.iterrows():
@@ -39,100 +31,90 @@ def main():
         rated = [(x, z) for x, y, z in combine if str(y) != 'nan']
         row_names = [i[0] for i in rated]
         testing_data_user_items[i] = row_names
-    print(testing_data_user_items)
-#################################################################################
-    # print(testing_data.groupby(testing_data.reviewerID).get_group("AQP1VPK16SVWM"))
-    # rating prediction class using user based collaborative filtering
-    # ubcf = prediction.UserBasedCF(training_data.reviewerID, training_data.asin, training_data.overall)
-    # user_id_list, item_id_list, rating_list, uir, user_set, item_set = ubcf.prep()
-    # user_id_list = list(OrderedDict.fromkeys(user_id_list))
-    # nn = ubcf.nearest_neighbors(user_id_list, item_id_list, rating_list, uir, user_set, item_set)
-    # # prediction = ubcf.predict(user_id_list, nn)
-    # print(nn)
-    missing_rating = training_data.pivot_table(values='overall', index='reviewerID', columns='asin')
-    # print(missing_rating.head())
-    rate = {}
+    # Create a dictionary to include all users in the training set with their rated items as values for their ids
+    training_pivot_rating = training_data.pivot_table(values='overall', index='reviewerID', columns='asin')
+    # A dictionary with users as keys and lists of items rated as list of values
+    user_items_rated = {}
+    # To store indices
     rows_indexes = {}
-    for i, row in missing_rating.iterrows():
-        rows = [x for x in range(0, len(missing_rating.columns))]
+    for i, row in training_pivot_rating.iterrows():
+        rows = [x for x in range(0, len(training_pivot_rating.columns))]
         combine = list(zip(row.index, row.values, rows))
         rated = [(x, z) for x, y, z in combine if str(y) != 'nan']
         index = [i[1] for i in rated]
         row_names = [i[0] for i in rated]
         rows_indexes[i] = index
-        rate[i] = row_names
+        user_items_rated[i] = row_names
+    # User item table
     pivot_table = training_data.pivot_table(values='overall', index='reviewerID', columns='asin').fillna(0)
     pivot_table = pivot_table.apply(np.sign)
-    # print(pivot_table.head())
-
-    notrated = {}
-    notrated_indexes = {}
-    for i, row in pivot_table.iterrows():
-        rows = [x for x in range(0, len(missing_rating.columns))]
-        combine = list(zip(row.index, row.values, row))
-        idx_row = [(idx, col) for idx, val, col in combine if not val > 0]
-        indices = [i[1] for i in idx_row]
-        row_names = [i[0] for i in idx_row]
-        notrated_indexes[i] = indices
-        notrated[i] = row_names
-    # print(notrated)
-
     # Nearest Neighbor Recommender
+    # Closest nearest neighbors to an item, I used the algorithm (brute) because it is faster
+    # I used the metric (cosine) because it measures similarity between vectors. Cosine measures
+    # the distance between two vectors. The closest the vectors are, the more correlated they are
+    # the farther the are, the more un correlated they are
     n = 10
     cosine_nn = NearestNeighbors(n_neighbors=n, algorithm='brute', metric='cosine')
     item_cosine_nn_fit = cosine_nn.fit(pivot_table.T.values)
+    # item indices give the index of the actual item
     item_distances, item_indices = item_cosine_nn_fit.kneighbors(pivot_table.T.values)
-
-    # Item Based Recommender
-    items_dic = {}
-    for i in range(len(pivot_table.T.index)):
-        item_idx = item_indices[i]
-        col_names = pivot_table.T.index[item_idx].tolist()
-        items_dic[pivot_table.T.index[i]] = col_names
-    # print(items_dic)
-
-    topRecs = {}
-    for k,v in rows_indexes.items():
-        item_idx = [j for i in item_indices[v] for j in i]
-        item_dist = [j for i in item_distances[v] for j in i]
-        combine = list(zip(item_dist, item_idx))
-        diction = {i: d for d, i in combine if i not in v}
-        zipped = list(zip(diction.keys(), diction.values()))
-        sort = sorted(zipped, key=lambda x: x[1])
-        recommendations = [(pivot_table.columns[i], d) for i, d in sort]
-        topRecs[k] = recommendations
-
-    # predictions
+    # The Predictions
     item_distances = 1 - item_distances
     predictions = item_distances.T.dot(pivot_table.T.values) / np.array([np.abs(item_distances.T).sum(axis=1)]).T
     ground_truth = pivot_table.T.values[item_distances.argsort()[0]]
 
     # Eval predictions
+    # Mean Absolute Error (MAE)
     def mae(prediction, ground_truth):
         prediction = prediction[ground_truth.nonzero()].flatten()
         ground_truth = ground_truth[ground_truth.nonzero()].flatten()
         return mean_absolute_error(prediction, ground_truth)
 
+    # Root Mean Square Error
     def rmse(prediction, ground_truth):
         prediction = prediction[ground_truth.nonzero()].flatten()
         ground_truth = ground_truth[ground_truth.nonzero()].flatten()
         return sqrt(mean_squared_error(prediction, ground_truth))
 
-    # error_rate = rmse(predictions, ground_truth)
-    # print("Accuracy: {:.3f}".format(100 - error_rate))
-    print("MAE: {:.5f}".format(mae(predictions, ground_truth)))
-    print("RMSE: {:.5f}".format(rmse(predictions, ground_truth)))
+    print("MAE: {:.2f}".format(mae(predictions, ground_truth)))
+    print("RMSE: {:.2f}".format(rmse(predictions, ground_truth)))
 
-    # making a 10-item list of recommendations to all users
-    def get_recommendations(user):
-        to_file = '\n\n'+user+' - '+str(rate[user])
+    # Item Based Recommender
+    items = {}
+    for i in range(len(pivot_table.T.index)):
+        item_index = item_indices[i]
+        # get the name of the index and turn it to a list
+        col_names = pivot_table.T.index[item_index].tolist()
+        # turn the list into a dictionary with keys of items and values of items that are associated with it
+        items[pivot_table.T.index[i]] = col_names
+    # Top recommendations
+    top_recommendations = {}
+    for userID, idx in rows_indexes.items():
+        # indices for all the items rated
+        item_index = [j for i in item_indices[idx] for j in i]
+        # distances for all the items rated
+        item_dist = [j for i in item_distances[idx] for j in i]
+        combine = list(zip(item_dist, item_index))
+        # filtering out the items that have been rated
+        dictionary = {i: d for d, i in combine if i not in idx}
+        zipped = list(zip(dictionary.keys(), dictionary.values()))
+        # list the similarity scores from the most similar to the least similar
+        # based of the items the user rated and items the user have not rated
+        sort = sorted(zipped, key=lambda x: x[1])
+        recommendations = [(pivot_table.columns[i], d) for i, d in sort]
+        top_recommendations[userID] = recommendations
+
+    # Making a 10-item list of recommendations to all users
+    def recommendations(user):
+        to_file = '\n\n'+user+' - '+str(user_items_rated[user])
         users_recommendations.write(to_file)
         count = 0
-        for a, b in topRecs.items():
+        for a, b in top_recommendations.items():
             if user == a:
                 for j in b[:10]:
                     to_file = '\n{} with similarity: {:.4f}'.format(j[0], 1 - j[1])
                     users_recommendations.write(to_file)
+                    # Check if i
                     if j[0] in testing_data_user_items.get(user):
                         count += 1
         items_count = len(testing_data_user_items.get(user))
@@ -145,7 +127,7 @@ def main():
     conversion_rate = 0
     for user in users:
         users_counter += 1
-        count_precision, count_recall = get_recommendations(user)
+        count_precision, count_recall = recommendations(user)
         sum_precision += count_precision
         sum_recall += count_recall
         if count_precision > 0:
@@ -159,10 +141,10 @@ def main():
     recall = (sum_recall / users_counter) * 100
     f_measure = 2 * precision * recall / (precision + recall)
     conversion_rate = (conversion_rate / users_counter) * 100
-    print('Precision {:.4f}'.format(precision))
-    print('Recall: {:.4f}'.format(recall))
-    print('F-measure: {:.4f}'.format(f_measure))
-    print('Conversion rate: {:.4f}'.format(conversion_rate))
+    print('Precision: {:.2f}%'.format(precision))
+    print('Recall: {:.2f}%'.format(recall))
+    print('F-measure: {:.2f}'.format(f_measure))
+    print('Conversion rate: {:.2f}%'.format(conversion_rate))
 
 
 if __name__ == "__main__":
